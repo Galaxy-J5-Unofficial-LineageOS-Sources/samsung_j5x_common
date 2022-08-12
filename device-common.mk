d2b377f96ac83d2b1da57f842701e1cc83c461ed
David <davidmolinamorales1999@gmail.com>
2022-08-12 22:06:39 +0200

    j5x-common: Define old makefile name

--- device-common.mk
+++ device-common.mk
@@ -6,7 +6,7 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Inherit from common
-$(call inherit-product, device/samsung/msm8916-common/lineage_msm8916.mk)
+$(call inherit-product, device/samsung/msm8916-common/msm8916.mk)
 
 # Inherit from vendor
 $(call inherit-product, vendor/samsung/j5x-common/j5x-common-vendor.mk)
